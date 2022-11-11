39bddf08b27811021b1af8f4b7cfcad94f2e2d5d
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2022-11-11 22:33:22 +0530

    Small change

--- flutter.js
+++ flutter.js
@@ -13,7 +13,7 @@ if (!_flutter) {
 }
 _flutter.loader = null;
 
-(function() {
+(function () {
   "use strict";
   class FlutterLoader {
     /**
@@ -31,7 +31,8 @@ _flutter.loader = null;
 
       // Called by Flutter web.
       // Bound to `this` now, so "this" is preserved across JS <-> Flutter jumps.
-      this.didCreateEngineInitializer = this._didCreateEngineInitializer.bind(this);
+      this.didCreateEngineInitializer =
+        this._didCreateEngineInitializer.bind(this);
     }
 
     /**
@@ -41,10 +42,7 @@ _flutter.loader = null;
      * or will be rejected with the error caused by the loader.
      */
     loadEntrypoint(options) {
-      const {
-        entrypointUrl = "main.dart.js",
-        serviceWorker,
-      } = (options || {});
+      const { entrypointUrl = "main.dart.js", serviceWorker } = options || {};
       return this._loadWithServiceWorker(entrypointUrl, serviceWorker);
     }
 
@@ -56,7 +54,9 @@ _flutter.loader = null;
      */
     _didCreateEngineInitializer(engineInitializer) {
       if (typeof this._didCreateEngineInitializerResolve != "function") {
-        console.warn("Do not call didCreateEngineInitializer by hand. Start with loadEntrypoint instead.");
+        console.warn(
+          "Do not call didCreateEngineInitializer by hand. Start with loadEntrypoint instead."
+        );
       }
       this._didCreateEngineInitializerResolve(engineInitializer);
       // Remove the public method after it's done, so Flutter Web can hot restart.
@@ -104,43 +104,46 @@ _flutter.loader = null;
 
     _loadWithServiceWorker(entrypointUrl, serviceWorkerOptions) {
       if (!("serviceWorker" in navigator) || serviceWorkerOptions == null) {
-        console.warn("Service worker not supported (or configured).", serviceWorkerOptions);
+        console.warn(
+          "Service worker not supported (or configured).",
+          serviceWorkerOptions
+        );
         return this._loadEntrypoint(entrypointUrl);
       }
 
-      const {
-        serviceWorkerVersion,
-        timeoutMillis = 4000,
-      } = serviceWorkerOptions;
+      const { serviceWorkerVersion, timeoutMillis = 4000 } =
+        serviceWorkerOptions;
 
-      let serviceWorkerUrl = "flutter_service_worker.js?v=" + serviceWorkerVersion;
-      let loader = navigator.serviceWorker.register(serviceWorkerUrl)
-          .then((reg) => {
-            if (!reg.active && (reg.installing || reg.waiting)) {
-              // No active web worker and we have installed or are installing
-              // one for the first time. Simply wait for it to activate.
-              let sw = reg.installing || reg.waiting;
+      let serviceWorkerUrl =
+        "flutter_service_worker.js?v=" + serviceWorkerVersion;
+      let loader = navigator.serviceWorker
+        .register(serviceWorkerUrl)
+        .then((reg) => {
+          if (!reg.active && (reg.installing || reg.waiting)) {
+            // No active web worker and we have installed or are installing
+            // one for the first time. Simply wait for it to activate.
+            let sw = reg.installing || reg.waiting;
+            return this._waitForServiceWorkerActivation(sw, entrypointUrl);
+          } else if (!reg.active.scriptURL.endsWith(serviceWorkerVersion)) {
+            // When the app updates the serviceWorkerVersion changes, so we
+            // need to ask the service worker to update.
+            console.debug("New service worker available.");
+            return reg.update().then((reg) => {
+              console.debug("Service worker updated.");
+              let sw = reg.installing || reg.waiting || reg.active;
               return this._waitForServiceWorkerActivation(sw, entrypointUrl);
-            } else if (!reg.active.scriptURL.endsWith(serviceWorkerVersion)) {
-              // When the app updates the serviceWorkerVersion changes, so we
-              // need to ask the service worker to update.
-              console.debug("New service worker available.");
-              return reg.update().then((reg) => {
-                console.debug("Service worker updated.");
-                let sw = reg.installing || reg.waiting || reg.active;
-                return this._waitForServiceWorkerActivation(sw, entrypointUrl);
-              });
-            } else {
-              // Existing service worker is still good.
-              console.debug("Loading app from service worker.");
-              return this._loadEntrypoint(entrypointUrl);
-            }
-          })
-          .catch((error) => {
-            // Some exception happened while registering/activating the service worker.
-            console.warn("Failed to register or activate service worker:", error);
+            });
+          } else {
+            // Existing service worker is still good.
+            console.debug("Loading app from service worker.");
             return this._loadEntrypoint(entrypointUrl);
-          });
+          }
+        })
+        .catch((error) => {
+          // Some exception happened while registering/activating the service worker.
+          console.warn("Failed to register or activate service worker:", error);
+          return this._loadEntrypoint(entrypointUrl);
+        });
 
       // Timeout race promise
       let timeout;
@@ -148,7 +151,11 @@ _flutter.loader = null;
         timeout = new Promise((resolve, _) => {
           setTimeout(() => {
             if (!this._scriptLoaded) {
-              console.warn("Loading from service worker timed out after", timeoutMillis, "milliseconds.");
+              console.warn(
+                "Loading from service worker timed out after",
+                timeoutMillis,
+                "milliseconds."
+              );
               resolve(this._loadEntrypoint(entrypointUrl));
             }
           }, timeoutMillis);
@@ -160,4 +167,4 @@ _flutter.loader = null;
   }
 
   _flutter.loader = new FlutterLoader();
-}());
+})();
